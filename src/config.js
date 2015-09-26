3b0bfba9dc3e2287b3bea81c3adab97570a1f363
Aldo <me@aldopadilla.com>
2015-09-25 23:44:52 -0400

    Initial
    Initial

---
+++ src/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  cloud: {
+    access_token: '46e5929deab7daaa7ee57986d993e6a5cce71e40782d01824ae1f23468bb7b84' //This is you Little Bits AccessToken under Advanced Settings
+  },
+  twitter: {
+    consumer_key: '8ctjELNzndDY4SiRoHU51uAnB',
+    consumer_secret: 'HuyRi6qzS2BJb7OTZ359PoLRYzMq9flvmwPUW1U9KosZjizBX2',
+    access_token_key: '17015006-F4Ih2SylOMLaW9WUaAZ66DiNemFydJDDZOqUgsLak',
+    access_token_secret: '0RQoWqoAioBbLWpRRIOryD9b8dvLkyQReFrr8G9gfi3KH'
+  },
+  mentions_duration: 5000,
+  retweet_duration: 3000,
+  twitter_users: [
+    { handle: '@mikaila1989',
+      cloudBit: '243c200c0308' }  //This is you Little Bits Device ID under Advanced Settings
+      ,
+    { handle: '@stresslimit',
+      cloudBit: 'bbb' }
+      ,
+    { handle: '@LabourNY',
+      cloudBit: 'ccc' }
+  ]
+
+}
